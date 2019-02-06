7937b865269d814bd73e289fc78dc45daf971d21
Johan Tötterman <johan.totterman@sprintit.fi>
2019-02-06 21:08:06 +0200

    Mode the Redis subscription listener outside the socket connection function.

--- events/index.js
+++ events/index.js
@@ -5,52 +5,49 @@ const redis_address = process.env.REDIS_ADDRESS || 'redis://127.0.0.1:6379'
 const pub = new Redis(redis_address)
 const sub = new Redis(redis_address)
 
-sub.subscribe('new_message', 'new_room', 'join_room', 'leave_room')
+sub.subscribe('new_chat', 'new_room')
 
 import { messages as messages_action } from '../actions'
-const { listMessages, writeMessage } = messages_action
+const { listMessages } = messages_action
 
 import { rooms as rooms_action } from '../actions'
-const { listRooms } = rooms_action
+const { listRooms, joinRoom } = rooms_action
 
 export const events = io => {
   // Socket.IO
   io.on('connection', socket => {
-    socket.on('join_room', room => {
+    socket.on('join_room', async room => {
+      // Join room
       socket.join(room)
+      const username = socket.request.session.passport.user
+      await joinRoom(room, username, socket.id)
 
       // Send messages history when joining room
-      listMessages(room).then(messages => {
-        socket.emit('list_messages', messages)
-      })
+      const messages = await listMessages(room)
+      socket.emit('list_messages', messages)
     })
 
-    listRooms().then(rooms => {
+    socket.on('list_rooms', async () => {
+      const rooms = await listRooms()
       socket.emit('list_rooms', rooms)
-    })
-
-    socket.on('write_message', messageData => {
-      const msgRoom = messageData.room
-      const msgUser = socket.request.session.passport.user
-      const msgDate = messageData.date
-      const msgMessage = messageData.message
-
-      /* Write message to database and publish it */
-      writeMessage(msgRoom, msgUser, msgDate, msgMessage).then(
-        pub.publish('new_message', JSON.stringify(messageData))
-      )
-    })
-
-    /*
-      Emit message to clients when someone publishes
-      to any of our subscribed channles
-    */
-    sub.on('message', (channel, message) => {
-      socket.emit(channel, message)
     })
 
     socket.on('disconnect', () => {
       console.log('user disconnected')
     })
+  })
+
+  /*
+    Emit message to clients when someone publishes
+    to any of our subscribed channels
+  */
+  sub.on('message', async (channel, message) => {
+    if (channel == 'new_chat') {
+      /* Chat messages are emitted only to specific room */
+      const mess = JSON.parse(message)
+      io.in(mess.room).emit(channel, message)
+    } else {
+      io.emit(channel, message)
+    }
   })
 }
