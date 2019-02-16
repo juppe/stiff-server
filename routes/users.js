4dd26289b0fc6139f6456990761480ef4e5e58be
Johan Tötterman <johan.totterman@sprintit.fi>
2019-02-16 23:47:03 +0200

    Authenticate new user right after it is created

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+import Redis from 'ioredis'
+import passport from 'passport'
 import { requireAuth } from '../auth'
-
-var router = Router()
-
 import { users as users_action } from '../actions'
+
+const redis_address = process.env.REDIS_ADDRESS || 'redis://127.0.0.1:6379'
 const { createUser, listUsers } = users_action
+
+const router = Router()
+const pub = new Redis(redis_address)
 
 /* GET users */
 router.get('/', requireAuth(), async (req, res) => {
@@ -13,13 +17,34 @@ router.get('/', requireAuth(), async (req, res) => {
 })
 
 /* CREATE user */
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   const result = await createUser(
     req.body.username,
     req.body.nickname,
     req.body.password
   )
-  res.send({ response: result })
+
+  await pub.publish(
+    'new_user',
+    JSON.stringify({
+      username: req.body.username,
+      nickname: req.body.nickname
+    })
+  )
+
+  passport.authenticate('local', (err, user) => {
+    if (err || !user) {
+      console.log('Login unsuccessful!')
+    }
+    req.login(user, err => {
+      if (err) {
+        console.log('Login unsuccessful!' + err)
+      }
+      res.status(200).json({
+        status: 'Login successful!'
+      })
+    })
+  })(req, res, next)
 })
 
 export const users = router
